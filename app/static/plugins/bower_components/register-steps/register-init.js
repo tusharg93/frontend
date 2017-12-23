ba221ee0aabd82c27378528d65267a66f318806b
bvhemanth1993 <hemanth@loktra.com>
2017-12-24 00:18:24 +0530

    first page

--- app/static/plugins/bower_components/register-steps/register-init.js
+++ app/static/plugins/bower_components/register-steps/register-init.js
@@ -1,3 +1,7 @@
+var host_api='http://52.77.252.230:8000/';
+var base_path='api/v1/';
+
+
 $(function() {
 
 //jQuery time
@@ -11,7 +15,13 @@ $(".next_1").click(function(e){
 	var currency=document.getElementById('currency').value;
 	var zone=document.getElementById('timezone').value;
 	var teatime=document.getElementById('teatime').value;
+	var duration=document.getElementById('duration').value;
 	//var tea=teatime.options[teatime.selectedIndex].value;
+	var nine='false';
+	var eighteen='false';
+	var online='false';
+	var guest='false';
+	var member='false';
 	console.log(currency+" ,"+zone+" ,"+teatime);
 
 	//var doc = document.getElementById("language");
@@ -20,8 +30,108 @@ $(".next_1").click(function(e){
 	if(animating) return false;
 	animating = true;
 	
+	if ($('#9').is(":checked"))
+	{
+	  	// it is checked
+	  	nine='True';
+	}
+
+	if ($('#18').is(":checked"))
+	{
+		eighteen='True';
+	  	// it is checked
+	}
+	if ($('#'+rates[0]['name']).is(":checked"))
+	{
+		online='True';
+	  	// it is checked
+	}
+	if ($('#'+rates[1]['name']).is(":checked"))
+	{
+		guest='True';
+	  	// it is checked
+	}
+	if ($('#'+rates[2]['name']).is(":checked"))
+	{
+		member='True';
+	  	// it is checked
+	}
+	//var lis = document.getElementById("teatime").getElementsByTagName("li");
+	//console.log(lis);
+	//const navbar = Array.from(document.querySelectorAll('#teatime>li'));
+
+	console.log($('#teatime').select2('data'));
+	var teeip=$('#teatime').select2('data');
+	var ratesfromlocal=localStorage.getItem('rates',rates);
+	console.log(ratesfromlocal);
+
+	var tee=[];
+
+	for(var i=0;i<teeip.length;i++)
+	{
+		tee.push(teeip[i]['id']);
+	}
+	var join=tee.join(',');
+	console.log(join);
+
+	var dataString=JSON.stringify({hole_9:nine,hole_18:eighteen,tee_avl:join,currency:currency,timezone:zone,member:member,guest:guest,online:online,duration:duration});		
+	console.log(dataString);
+	 $.ajax({
+	 	type:'post',
+	 	url:host_api+base_path+'forms/1',
+	 	contentType:"application/json",
+	 	data:dataString,
+	 	xhrFields: {
+	       	withCredentials: true
+	   	},
+	   	crossDomain: true,
+	 	success:function(data)
+	 	{
+	 		console.log(data)
+
+	 	},error:function(xhr,error,status){
+
+	 	}
+	 });
+
+	current_fs = $(this).parent();
+	next_fs = $(this).parent().next();
+	//activate next step on progressbar using the index of next_fs
+	$("#eliteregister li").eq($("form").index(next_fs)).addClass("active");
 	
-		current_fs = $(this).parent();
+		//show the next fieldset
+		next_fs.show(); 
+		//hide the current fieldset with style
+		current_fs.animate({opacity: 0}, {
+			step: function(now, mx) {
+				//as the opacity of current_fs reduces to 0 - stored in "now"
+				//1. scale current_fs down to 80%
+				scale = 1 - (1 - now) * 0.2;
+				
+				//2. bring next_fs from the right(50%)
+				left = (now * 50)+"%";
+				
+				//3. increase opacity of next_fs to 1 as it moves in
+				opacity = 1 - now;
+
+				current_fs.css({'transform': 'scale('+scale+')'});
+				next_fs.css({'left': left, 'opacity': opacity});
+			}, 
+			duration: 800, 
+			complete: function(){
+				current_fs.hide();
+				animating = false;
+			}, 
+			//this comes from the custom easing plugin
+			easing: 'easeInOutBack'
+		});
+});
+
+
+$(".next_2").click(function(e){
+	console.log(e);
+
+	current_fs = $(this).parent();
 	next_fs = $(this).parent().next();
 	//activate next step on progressbar using the index of next_fs
 	$("#eliteregister li").eq($("form").index(next_fs)).addClass("active");
